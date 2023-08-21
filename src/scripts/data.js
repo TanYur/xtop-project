5383e3410a01db6711b50dd80dde015acbbbdee8
TanaYur <8549tan.y@gmail.com>
2023-08-21 15:42:19 +0200

    Update data.js

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,1 +1,63 @@
-
+const data = {
+    'bubbles':{
+        'Space Bubbles':{
+            title: `A revolution in space exploration and a new opportunity for alternative energy for the needs of
+            mankind.`,
+            subtitle: `Relevance and value of the product Space bubbles.`,
+            text: `Having studied a number of scientific papers, as well as having conducted a series of our own
+            experiments, our team came to the conclusion that at this stage of development,science remains
+            at the same place in many areas of astronautics and astrophysics...`,
+            link: `src/articles/space_bubbles.html`
+        }
+    },
+    'medicine': {
+        'Regenertion of body to a yuotful state':{
+            title: `Regenertion of body to a yuotful state title`,
+            subtitle: `Regenertion of body to a yuotful state subtitle`,
+            text: `Regenertion of body to a yuotful state content...`,
+            link: `src/articles/regenertion_of_body.html`
+        },
+        'Artificial vision': {
+            title: `Artificial vision title`,
+            subtitle: `Artificial vision subtitle`,
+            text: `Artificial vision content...`,
+            link: `src/articles/artificial_vision.html`
+        }, 
+        'Body regeneration': {
+            title: `Body regeneration title`,
+            subtitle: `Body regeneration subtitle`,
+            text: `Body regeneration content...`,
+            link: `src/articles/body_regeneration.html`
+        }
+    },
+    'ecology':{
+        'Cleaning the surface of the water from the oil film':{
+            title: `Cleaning the surface of the water from the oil film title`,
+            subtitle: `Cleaning the surface of the water from the oil film subtitle`,
+            text: `Cleaning the surface of the water from the oil film content...`,
+            link: `src/articles/cleaning_the_water.html`
+        },
+        'Artificial lung of the Earth':{
+            title: `The Artificial Lungs of the Planet project has overarching goals to address a range of environmental and accessibility issues.`,
+            subtitle: ``,
+            text: `Having studied a number of scientific papers, as well as having conducted a series of our own
+            experiments, our team came to the conclusion that at this stage of development,science remains
+            at the same place in many areas of astronautics and astrophysics...`,
+            link: `src/articles/artificial_lungs_planet.html`
+        },
+        'Wind generator on sails':{
+            title: `Wind generator on sails title`,
+            subtitle: `Wind generator on sails subtitle`,
+            text: `Wind generator on sails content...`,
+            link: `src/articles/wind_generator_on_sails.html`
+        },
+    }, 
+    'mirror': {
+        '3d mirror World': {
+            title: `3d mirror World title`,
+            subtitle: `3d mirror World subtitle`,
+            text: `3d mirror World content...`,
+            link: `src/articles/3d_mirror_world.html`
+        }
+    }
+};
