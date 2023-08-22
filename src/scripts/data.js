0d91f671671d7d925b99e0d9629c1b6d0a95c4d4
TanaYur <8549tan.y@gmail.com>
2023-08-22 12:02:13 +0200

    Update data.js

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,63 +1,60 @@
 const data = {
     'bubbles':{
-        'Space Bubbles':{
-            title: `A revolution in space exploration and a new opportunity for alternative energy for the needs of
-            mankind.`,
-            subtitle: `Relevance and value of the product Space bubbles.`,
-            text: `Having studied a number of scientific papers, as well as having conducted a series of our own
-            experiments, our team came to the conclusion that at this stage of development,science remains
-            at the same place in many areas of astronautics and astrophysics...`,
-            link: `src/articles/space_bubbles.html`
-        }
+        'Space Bubbles':
+           `<h4>A revolution in space exploration and a new opportunity for alternative energy for the needs of
+           mankind.</h4>
+           <h5>Relevance and value of the product Space bubbles.</h5>
+           <p>Having studied a number of scientific papers, as well as having conducted a series of our own
+           experiments, our team came to the conclusion that at this stage of development,science remains
+           at the same place in many areas of astronautics and astrophysics...</p>
+           <button class="btn"><a href="src/articles/space_bubbles.html">view more &#8594;</a></button>
+           `                  
     },
     'medicine': {
-        'Regenertion of body to a yuotful state':{
-            title: `Regenertion of body to a yuotful state title`,
-            subtitle: `Regenertion of body to a yuotful state subtitle`,
-            text: `Regenertion of body to a yuotful state content...`,
-            link: `src/articles/regenertion_of_body.html`
-        },
-        'Artificial vision': {
-            title: `Artificial vision title`,
-            subtitle: `Artificial vision subtitle`,
-            text: `Artificial vision content...`,
-            link: `src/articles/artificial_vision.html`
-        }, 
-        'Body regeneration': {
-            title: `Body regeneration title`,
-            subtitle: `Body regeneration subtitle`,
-            text: `Body regeneration content...`,
-            link: `src/articles/body_regeneration.html`
-        }
+        'Regenertion of body to a yuotful state':
+        `<h4>Regenertion of body to a yuotful state title</h4>
+        <h5>Regenertion of body to a yuotful state subtitle</h5>
+        <p>Regenertion of body to a yuotful state content...</p>
+        <button class="btn"><a href="src/articles/regenertion_of_body.html">view more &#8594;</a></button>
+        ` ,        
+        'Artificial vision':       
+        `<h4>Artificial vision title</h4>
+        <h5>Artificial vision subtitle</h5>
+        <p>Artificial vision content...</p>
+        <button class="btn"><a href="src/articles/artificial_vision.html">view more &#8594;</a></button>
+        `,       
+        'Body regeneration':       
+        `<h4>Body regeneration title</h4>
+        <h5>Body regeneration subtitle</h5>
+        <p>Body regeneration content...</p>
+        <button class="btn"><a href="src/articles/body_regeneration.html">view more &#8594;</a></button>
+        `       
     },
     'ecology':{
-        'Cleaning the surface of the water from the oil film':{
-            title: `Cleaning the surface of the water from the oil film title`,
-            subtitle: `Cleaning the surface of the water from the oil film subtitle`,
-            text: `Cleaning the surface of the water from the oil film content...`,
-            link: `src/articles/cleaning_the_water.html`
-        },
-        'Artificial lung of the Earth':{
-            title: `The Artificial Lungs of the Planet project has overarching goals to address a range of environmental and accessibility issues.`,
-            subtitle: ``,
-            text: `Having studied a number of scientific papers, as well as having conducted a series of our own
-            experiments, our team came to the conclusion that at this stage of development,science remains
-            at the same place in many areas of astronautics and astrophysics...`,
-            link: `src/articles/artificial_lungs_planet.html`
-        },
-        'Wind generator on sails':{
-            title: `Wind generator on sails title`,
-            subtitle: `Wind generator on sails subtitle`,
-            text: `Wind generator on sails content...`,
-            link: `src/articles/wind_generator_on_sails.html`
-        },
-    }, 
+        'Cleaning the surface of the water from the oil film':
+        `<h4>Cleaning the surface of the water from the oil film title</h4>
+        <h5>Cleaning the surface of the water from the oil film subtitle</h5>
+        <p>Cleaning the surface of the water from the oil film content...</p>
+        <button class="btn"><a href="src/articles/cleaning_the_water.html">view more &#8594;</a></button>`,       
+        'Artificial lung of the Earth':
+        `<h4>The Artificial Lungs of the Planet project has overarching goals to address a range of environmental and accessibility issues.</h4>        
+         <p>Having studied a number of scientific papers, as well as having conducted a series of our own
+         experiments, our team came to the conclusion that at this stage of development,science remains
+         at the same place in many areas of astronautics and astrophysics...</p>
+         <button class="btn"><a href="src/articles/artificial_lungs_planet.html">view more &#8594;</a></button>`,       
+        'Wind generator on sails':
+         `<h4>Wind generator on sails title</h4>
+        <h5>Wind generator on sails subtitle</h5>
+        <p>Wind generator on sails content...</p>
+        <button class="btn"><a href="src/articles/wind_generator_on_sails.html">view more &#8594;</a></button>` 
+        }, 
     'mirror': {
-        '3d mirror World': {
-            title: `3d mirror World title`,
-            subtitle: `3d mirror World subtitle`,
-            text: `3d mirror World content...`,
-            link: `src/articles/3d_mirror_world.html`
-        }
+        '3d mirror World':
+        `<h4>3d mirror World title</h4>
+        <h5>3d mirror World subtitle</h5>
+        <p>3d mirror World content...</p>
+        <button class="btn"><a href="src/articles/3d_mirror_world.html">view more &#8594;</a></button>`       
     }
 };
+
+
