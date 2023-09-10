f7b37e0891df1b410bc3631cad21c3afbf0f39fa
TanaYur <8549tan.y@gmail.com>
2023-09-10 19:16:09 +0200

    regenertion of body a yuotful state

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,60 +1,61 @@
 const data = {
-    'bubbles':{
+    'bubbles': {
         'Space Bubbles':
-           `<h4>A revolution in space exploration and a new opportunity for alternative energy for the needs of
+            `<h4>A revolution in space exploration and a new opportunity for alternative energy for the needs of
            mankind.</h4>
            <h5>Relevance and value of the product Space bubbles.</h5>
            <p>Having studied a number of scientific papers, as well as having conducted a series of our own
            experiments, our team came to the conclusion that at this stage of development,science remains
            at the same place in many areas of astronautics and astrophysics...</p>
            <button class="btn"><a href="src/articles/space_bubbles.html">view more &#8594;</a></button>
-           `                  
+           `
     },
     'medicine': {
         'Regenertion of body to a yuotful state':
-        `<h4>Regenertion of body to a yuotful state title</h4>
-        <h5>Regenertion of body to a yuotful state subtitle</h5>
-        <p>Regenertion of body to a yuotful state content...</p>
+            `<h4>Regenertion of body to a yuotful state</h4>                     
+        <p>Human life consists of seven stages in the cycle of development: childhood up to 12
+            years, adolescence up to 24, youth up to 36, maturity up to 42, preparation for old age up to 64, old age
+            up to 84, and deep old age. In total, there are 7 cycles, each lasting 12 years, with variations...</p>
         <button class="btn"><a href="src/articles/regenertion_of_body.html">view more &#8594;</a></button>
-        ` ,        
-        'Artificial vision':       
-        `<h4>Artificial Vision.</h4>       
+        ` ,
+        'Artificial vision':
+            `<h4>Artificial Vision.</h4>       
         <p><span>Problem:</span> The challenge we aim to solve concerns individuals who have difficulty seeing
         or are completely blind. Their limited vision affects their everyday lives, limiting their
         independence and opportunities. Existing solutions don't offer a complete substitute...</p>
         <button class="btn"><a href="src/articles/artificial_vision.html">view more &#8594;</a></button>
-        `,       
-        'Body regeneration':       
-        `<h4>Body regeneration title</h4>
+        `,
+        'Body regeneration':
+            `<h4>Body regeneration title</h4>
         <h5>Body regeneration subtitle</h5>
         <p>Body regeneration content...</p>
         <button class="btn"><a href="src/articles/body_regeneration.html">view more &#8594;</a></button>
-        `       
-    }, 
-    'ecology':{
+        `
+    },
+    'ecology': {
         'Cleaning the surface of the water from the oil film':
-        `<h4>Cleaning the surface of the water from the oil film title</h4>
+            `<h4>Cleaning the surface of the water from the oil film title</h4>
         <h5>Cleaning the surface of the water from the oil film subtitle</h5>
         <p>Cleaning the surface of the water from the oil film content...</p>
-        <button class="btn"><a href="src/articles/cleaning_the_water.html">view more &#8594;</a></button>`,       
+        <button class="btn"><a href="src/articles/cleaning_the_water.html">view more &#8594;</a></button>`,
         'Artificial lung of the Earth':
-        `<h4>The Artificial Lungs of the Planet project has overarching goals to address a range of environmental and accessibility issues.</h4>        
+            `<h4>The Artificial Lungs of the Planet project has overarching goals to address a range of environmental and accessibility issues.</h4>        
          <p>Having studied a number of scientific papers, as well as having conducted a series of our own
          experiments, our team came to the conclusion that at this stage of development,science remains
          at the same place in many areas of astronautics and astrophysics...</p>
-         <button class="btn"><a href="src/articles/artificial_lungs_planet.html">view more &#8594;</a></button>`,       
+         <button class="btn"><a href="src/articles/artificial_lungs_planet.html">view more &#8594;</a></button>`,
         'Wind generator on sails':
-         `<h4>Wind generator on sails title</h4>
+            `<h4>Wind generator on sails title</h4>
         <h5>Wind generator on sails subtitle</h5>
         <p>Wind generator on sails content...</p>
-        <button class="btn"><a href="src/articles/wind_generator_on_sails.html">view more &#8594;</a></button>` 
-        }, 
+        <button class="btn"><a href="src/articles/wind_generator_on_sails.html">view more &#8594;</a></button>`
+    },
     'mirror': {
         '3d mirror World':
-        `<h4>3d mirror World title</h4>
+            `<h4>3d mirror World title</h4>
         <h5>3d mirror World subtitle</h5>
         <p>3d mirror World content...</p>
-        <button class="btn"><a href="src/articles/3d_mirror_world.html">view more &#8594;</a></button>`       
+        <button class="btn"><a href="src/articles/3d_mirror_world.html">view more &#8594;</a></button>`
     }
 };
 
