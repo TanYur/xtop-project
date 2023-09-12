af51773059cb2bcb151d8f5e381a424843c97399
TanaYur <8549tan.y@gmail.com>
2023-09-12 11:02:45 +0200

    Update sub-page.js

--- src/scripts/sub-page.js
+++ src/scripts/sub-page.js
@@ -2,18 +2,33 @@
 
 const bntBackSubPage = document.querySelector('.btn-back');
 const linkedinBtnSubPage = document.querySelector('.sub-page .linkedin');
+const copyBtns = document.querySelectorAll('.project-help-page li .btn');
 bntBackSubPage.remove();
 
 const newbtn = bntBackSubPage.outerHTML;
 linkedinBtnSubPage.insertAdjacentHTML('beforeBegin', newbtn);
 
-function myFunction() {   
-    var copyText = document.querySelector(".copy-number"); 
+
+copyBtns.forEach((i, index) => {
+  i.addEventListener('click', function () {
+    let parentBtn = document.querySelectorAll('.project-help-page li');    
+    var copyText = parentBtn[index].querySelector(".copy-number");    
+    const copyBtns = document.querySelectorAll('.project-help-page li .btn');
+    copyBtns[index].classList.add('btn-copy');
     copyText.select();
     document.execCommand("copy");
-    alert('copy');
-  }
+  })
+});
 
-  document.addEventListener('click', myFunction);
- 
-
+copyBtns.forEach((i) => {
+  i.onmouseout = function (event) {
+    let target = event.target;
+    target.classList.remove('btn-copy');
+  };
+})
+copyBtns.forEach((i, index) => {
+  i.pointerout = function (event) {
+    let target = event.target;
+    target.classList.remove('btn-copy');
+  };
+})
